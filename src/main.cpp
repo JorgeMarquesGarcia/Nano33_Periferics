9f9ba04696b373b69a5607cd23bc9e212f74ebc6
Jorge Marqués García <jmarquesgarcia@unizar.es>
2025-02-25 20:28:03 +0100

    ADC interrupt each 10 seconds.
    The main idea of the interrupt has been taken from the library NRF52_MBED_TimerInterrupt, from khoih-prog, in particular
    from https://github.com/khoih-prog/NRF52_MBED_TimerInterrupt/tree/main/examples/TimerInterruptLEDDemo
    
    The problem with this library is that ISR timer do not support complex problems in handler or will be hang (really proven)
    so I decided to make a trigger in the handler and the read and write task in the loop.

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,73 @@
 #include <Arduino.h>
+#if !( ARDUINO_ARCH_NRF52840 && TARGET_NAME == ARDUINO_NANO33BLE )
+  #error This code is designed to run on nRF52-based Nano-33-BLE boards using mbed-RTOS platform! Please check your Tools->Board setting.
+#endif
 
-// put function declarations here:
-extern const int adcPin = P0_4;
-const int delayTime = 1000;
-char buffer[50];
+#define TIMER_INTERRUPT_DEBUG         0
+#define _TIMERINTERRUPT_LOGLEVEL_     0
 
-void readADC(const int &delayTime) {
-  int adcValue = analogRead(adcPin);
-  // Serial.println(adcValue);
-  sprintf(buffer,"ADC read value: %d", adcValue);
-  Serial.println(buffer);
-  // Serial.print("ADC read value: ");
-  // Serial.println(adcValue);
-  delay(delayTime);
+#include "NRF52_MBED_TimerInterrupt.h"
+#include "NRF52_MBED_ISR_Timer.h"
+
+#define HW_TIMER_INTERVAL_MS      1000  // Intervalo del temporizador de hardware en milisegundos (1 segundo)
+#define TIMER_INTERVAL_10S        10000L  // Intervalo del temporizador basado en ISR en milisegundos (10 segundos)
+
+NRF52_MBED_Timer ITimer(NRF_TIMER_3);
+NRF52_MBED_ISRTimer ISR_Timer;
+
+#ifndef LED_BLUE_PIN
+  #if defined(LEDB)
+    #define LED_BLUE_PIN          LEDB
+  #else
+    #define LED_BLUE_PIN          D7
+  #endif
+#endif
+
+void TimerHandler()
+{
+  ISR_Timer.run();
 }
 
+extern const int adcPin = P0_4;
+volatile bool adcTrigger = false;
+
+void triggerADC() {
+  adcTrigger = true;
+  digitalWrite(LED_BLUE_PIN, !digitalRead(LED_BLUE_PIN));
+}
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  while (!Serial && millis() < 5000);
+  delay(100);
+
+  Serial.print(F("\nStarting TimerInterruptLEDDemo on "));
+  Serial.println(BOARD_NAME);
+  Serial.println(NRF52_MBED_TIMER_INTERRUPT_VERSION);
+
   pinMode(adcPin, INPUT);
-  // put your setup code here, to run once:
+  pinMode(LED_BLUE_PIN, OUTPUT);
+
+  if (ITimer.attachInterruptInterval(HW_TIMER_INTERVAL_MS *1000 , TimerHandler))
+  {
+    Serial.print(F("Starting ITimer OK, millis() = "));
+    Serial.println(millis());
+  }
+  else
+    Serial.println(F("Can't set ITimer. Select another freq. or timer"));
+
+  ISR_Timer.setInterval(TIMER_INTERVAL_10S,  triggerADC);
 }
 
 void loop() {
-  readADC(delayTime);
+    if (adcTrigger) {
+    adcTrigger = false;
+    int adcValue = analogRead(adcPin);
+    char buffer[50];
+    sprintf(buffer, "ADC read value: %d", adcValue);
+    Serial.println(buffer);
+  }
 }
-
-// put function definitions here:
-
 
 
 
